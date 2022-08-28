49286905a98bd75cbe70cc1be21ed0daed90a27c
IgorTheOverlord <102486458+IgorTheOverlord@users.noreply.github.com>
2022-08-28 12:32:19 +0300

    Update bash_most_common.py

--- bash_most_common.py
+++ bash_most_common.py
@@ -2,27 +2,28 @@ import getpass
 from sys import platform
 from collections import Counter
 
-user_login = getpass.getuser()
-
 
 def bash_history():
+    
+    user_login = getpass.getuser()
+
     if platform == 'linux':
-        bash_file = f"/home/{user_login}/.bash_history"
-    if platform == 'darwin':
+        bash_file = f"/home/{user_login}/.bash_history" 
+    elif platform == 'darwin':
         bash_file = f"/users/{user_login}/.bash_history"
-    values = []
+    else:
+        return print("Doesn't work on windows")
+
     try:
-        with open(bash_file, encoding="ISO-8859-15") as b:
-            for line in b.read().splitlines():
-                values.append(line)
+        with open(bash_file, encoding="ISO-8859-15") as bash:
+            for k, v in Counter(bash).most_common()[::-1]:
+                print(f"{k} was used {v} times")
+
     except FileNotFoundError:
         if platform == 'linux':
             return print("No file '.bash_history' was found in home directory")
         if platform == 'darwin':
             return print("No file '.bash_history' was found in users directory")
-    else:
-        for k, v in Counter(values).most_common()[::-1]:
-            print(f"{k}, \033[1m{v} times\033[0m")
 
 
 bash_history()
