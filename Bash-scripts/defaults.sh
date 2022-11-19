c928801cf0a0249fd07e128c9b202fa5a10b1d31
IgorTheOverlord <102486458+IgorTheOverlord@users.noreply.github.com>
2022-11-19 12:43:28 +0300

    Update defaults.sh

--- Bash-scripts/defaults.sh
+++ Bash-scripts/defaults.sh
@@ -80,8 +80,11 @@ if [[ "$answer" == 'y' || "$answer" == 'Y' ]]; then
 
 fi
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i 's/plugins=[^\]/plugins=(zsh-syntax-highlighting /g' "$HOME/.zshrc"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i 's/plugins=[^\]/plugins=(zsh-autosuggestions /g' "$HOME/.zshrc"
 
 sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
